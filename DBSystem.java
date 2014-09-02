a303162a772b57421866634b11046787283c4062
thewayofknowing <thewayofknowing@gmail.com>
2014-09-03 00:54:19 +0530

    InitConfig Kartik

--- DBSystem.java
+++ DBSystem.java
@@ -1,8 +1,14 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
+import java.util.Vector;
 
 /* Code belongs to Kartik Kohli & Jigar Thakkar :P */
 
@@ -10,16 +16,61 @@ public class DBSystem {
 	
 	final int CacheSize = 100;
 	HashMap<String, Integer> tableNameToInt = new HashMap<>();
-	List<List<Pair>> listOfMaps = new ArrayList<List<Pair>>();
+	Vector<Vector<Pair>> listOfMaps = new Vector<Vector<Pair>>();
 	List<Page> pages = new ArrayList<Page>();
-	int pageSize,numPages;
+	int PAGESIZE,NUM_PAGES;
+	String PATH_FOR_DATA;
 	int numberOfTables;
+	Vector<Integer> x = new Vector<>();
 	Queue<Page> cache = new LinkedList<Page>();
 	
 	public void readConfig(String configFilePath) {
+		BufferedReader br = null;
+		String str = "";
+		try {
+			String currentLine;
+			br = new BufferedReader(new FileReader(configFilePath));
+			while((currentLine = br.readLine()) != null) {
+				str += currentLine;
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String[] params = str.split("\n");
+		
+		//Extracting Parameters from config file    
+		PAGESIZE = Integer.valueOf(params[0].split(" ")[1]); 
+		NUM_PAGES = Integer.valueOf(params[1].split(" ")[1]);
+		PATH_FOR_DATA = params[2].split(" ")[1];
+		int i=3,tableCount=0;
+		while(i<params.length) {
+			tableNameToInt.put(params[++i], tableCount++);
+			while(params[++i].equals("END")==false) {
+				//REad TABle attributes
+			}
+		}	
 	}
 
 	public void populatePageInfo() {
+		BufferedReader br = null;
+		try {
+			String currentLine;
+			for(Map.Entry<String, Integer> entry: tableNameToInt.entrySet()) {
+				br = new BufferedReader(new FileReader(PATH_FOR_DATA + entry.getKey()));
+				while((currentLine = br.readLine())!=null) {
+					
+				}
+			}
+				
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public String getRecord(String tableName, int recordId) {
