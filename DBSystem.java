422723ae23a30acd780bfd9a63c4dcf7373ae7f4
thewayofknowing <thewayofknowing@gmail.com>
2014-09-02 23:50:08 +0530

    Skeleton

--- DBSystem.java
+++ DBSystem.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /* Code belongs to Kartik Kohli & Jigar Thakkar :P */
 
 public class DBSystem {
+	
+	final int CacheSize = 100;
+	HashMap<String, Integer> tableNameToInt = new HashMap<>();
+	List<List<Pair>> listOfMaps = new ArrayList<List<Pair>>();
+	List<Page> pages = new ArrayList<Page>();
+	int pageSize,numPages;
+	int numberOfTables;
+	Queue<Page> cache = new LinkedList<Page>();
+	
 	public void readConfig(String configFilePath) {
 	}
 
@@ -9,5 +24,13 @@ public class DBSystem {
 
 	public String getRecord(String tableName, int recordId) {
 		return "record";
+	}
+	
+	public static class Pair {
+		int start,end;
+	}
+	
+	public static class Page {
+		String page;
 	}
 }
