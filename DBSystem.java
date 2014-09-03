c51d447d06d5ec9cf3cc1cc10188ddac2207278d
Jigar Thakkar <jthakkar5494@gmail.com>
2014-09-03 23:46:07 +0530

    Jigar Final Code

--- DBSystem.java
+++ DBSystem.java
@@ -24,7 +24,7 @@ public class DBSystem {
         public void readConfig(String configFilePath) {
                 BufferedReader br = null;
                 String str = "";
-                System.out.println(configFilePath);
+                //System.out.println(configFilePath);
                 //String[] params = new String[1000];
                 int ctr = 0;
                 try {
@@ -40,51 +40,57 @@ public class DBSystem {
                         e.printStackTrace();
                 }
                 String[] params = str.split("\n");
-                System.out.println(params[0]);
+                //System.out.println(params[0]);
                 //Extracting Parameters from config file    
                 PAGESIZE = Integer.valueOf(params[0].split(" ")[1]); 
                 NUM_PAGES = Integer.valueOf(params[1].split(" ")[1]);
                 PATH_FOR_DATA = params[2].split(" ")[1];
-                System.out.println(PAGESIZE + " " + NUM_PAGES + " " + PATH_FOR_DATA);
+                //System.out.println(PAGESIZE + " " + NUM_PAGES + " " + PATH_FOR_DATA);
                 int i=3,tableCount=0;
-                System.out.println(params.length+"");
+                //System.out.println(params.length+"");
                 while(i<params.length) {
                         //if i > parax
                         tableNameToInt.put(params[++i], tableCount++);
                         Vector<Page> vectorPage = new Vector<Page>();
-                        vectorPage.add(new Page(0, 0, 0, 0));
+                        vectorPage.add(new Page(0, 0, 0, 0, params[i]));
+                        //System.out.println(table.toString());
                         listOfMaps.add(vectorPage);
                         while(params[++i].equals("END")==false) {
                                 //REad TABle attributes
-                                System.out.println(params[i]);
+                                //System.out.println(params[i]);
                         }
                         i++;
-                        System.out.println(i+"");
+                        //System.out.println(i+"");
                 }	
         }
 
         public void populateDBInfo() {
                 BufferedReader br = null;
-                System.out.println(tableNameToInt.size() + "");
                 try {
                         String currentLine;
                         for(Map.Entry<String, Integer> entry: tableNameToInt.entrySet()) {
                                 br = new BufferedReader(new FileReader(PATH_FOR_DATA + entry.getKey() + ".csv"));
-                                System.out.println(entry.getKey());
+                                //System.out.println(entry.getKey());
                                 Vector<Page> table = listOfMaps.get(entry.getValue());
                                 int start=0,end=0,currentSize=0,pageNumber=0;
                                 Page currentPage = table.get(pageNumber);
                                 while((currentLine = br.readLine())!=null) {
-                                		currentPage.currentSize += currentLine.length();
-                                        if(currentPage.currentSize>PAGESIZE) {
-                                        	    if(pageNumber==0) table.get(pageNumber).limit.end = end;
-                                                table.add(new Page(pageNumber++,entry.getValue(),start,end));
+                                        //System.out.println("size:" + currentPage.currentSize);
+
+                                        if(currentPage.currentSize + currentLine.length()>PAGESIZE) {
+                                                //System.out.println(start + ":" + end);
+                                                //if(pageNumber==0)
+                                                //{
+                                                //        table.get(pageNumber).limit.end = end;
+                                                //}
+                                                start=end; 
+                                                table.add(new Page(++pageNumber,entry.getValue(),start, end, entry.getKey()));
+                                                //System.out.println(table.toString());
                                                 currentPage = table.get(pageNumber);
-                                                start=end+1;
                                         }
                                         else {
-                                                table.get(pageNumber).limit.end = end;
-                                                end++;
+                                                currentPage.currentSize += currentLine.length();
+                                                table.get(pageNumber).limit.end = ++end;
                                         }
                                 }
                         }
@@ -105,12 +111,12 @@ public class DBSystem {
                 while( first <= last )
                 {
                         Page midPage = listOfMaps.get(tableNumber).get(middle);
-                        System.out.println(midPage.limit.start + "End: " + midPage.limit.end);
+                        //System.out.println(midPage.limit.start + "End: " + midPage.limit.end);
                         if ( midPage.limit.end < recordId )
                                 first = middle + 1;    
                         else if ( midPage.limit.start <= recordId && midPage.limit.end >= recordId) 
                         {
-                                System.out.println("here: \n" + recordId + "\n");
+                                //System.out.println("here: \n" + recordId + "\n");
                                 //System.out.println(search + " found at location " + (middle + 1) + ".");
                                 if (cache.contains(midPage))
                                 {
@@ -147,26 +153,75 @@ public class DBSystem {
                 }
                 return "record";
         }
-        
+
         public void insertRecord(String tableName, String record) {
-        	int tableNumber = tableNameToInt.get(tableName);
-        	int tablePages = listOfMaps.get(tableNumber).size();
-        	Page lastPage = listOfMaps.get(tableNumber).get(tablePages - 1);
-                System.out.println(lastPage.limit.end + "");
-        	lastPage.currentSize += record.length(); 
-        	if (lastPage.currentSize <= PAGESIZE)
-        	{
-        		lastPage.limit.end ++;
-        	}
-        	else
-        	{
-        		listOfMaps.get(tableNumber).add(new Page(lastPage.pageNumber + 1, lastPage.tableNumber, 0, 0));
-        		Page addedPage = listOfMaps.get(tableNumber).get(tablePages);
-        		addedPage.limit.end ++;
-        	}
-                System.out.println(lastPage.limit.end + "");
+                int tableNumber = tableNameToInt.get(tableName);
+                int tablePages = listOfMaps.get(tableNumber).size();
+                Page lastPage = listOfMaps.get(tableNumber).get(tablePages - 1);
+                //System.out.println(lastPage.limit.end + "");
+                lastPage.currentSize += record.length(); 
+                if (lastPage.currentSize <= PAGESIZE)
+                {
+                        lastPage.page = record;
+                        if (cache.contains(lastPage))
+                        {
+                                cache.remove(lastPage);
+                                System.out.println("HIT in insert");
+                        }
+                        else
+                        {
+                                //cache.add(lastPage);
+                                System.out.println("MISSED during insert");
+                        }
+                        cache.add(lastPage);
+                        lastPage.limit.end ++;
+                }
+                else
+                {
+                        listOfMaps.get(tableNumber).add(new Page(lastPage.pageNumber + 1, lastPage.tableNumber, 0, 0, tableName));
+                        Page addedPage = listOfMaps.get(tableNumber).get(tablePages);
+                        addedPage.limit.end ++;
+                        addedPage.page = record;
+                        if (cache.size() < NUM_PAGES)
+                        {
+                                cache.add(addedPage);
+                                System.out.println("Newly created Page added to the cache!!");
+                        }
+                        else
+                        {
+                                Page replacedPage = cache.remove();
+                                cache.add(addedPage);
+                                System.out.println("Replaced a page in cache for the newly created page. Replaced Page: " + replacedPage.pageNumber);
+                        }
+                }
+                //System.out.println(lastPage.limit.end + "");
         }
-        
+        public void flushPages()
+        {
+
+                for (int i = 0;i < cache.size();i ++)
+                {
+                        //System.out.println("here");
+                        Page poppedPage = cache.remove();
+                        //System.out.println(poppedPage.page);
+                        try {
+                                String finalPath = PATH_FOR_DATA + poppedPage.tableName + ".csv";
+                                //System.out.println(finalPath);
+                                //PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter(finalPath, true)));
+                                if (poppedPage.page.length()!=0) {
+                                         PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter(finalPath, true)));
+                                        output.println(poppedPage.page);
+                                        output.close();
+                                }
+                                //output.write(poppedPage.page);
+                                cache.add(poppedPage);
+                        }
+                        catch(IOException e){
+                                System.out.println("Failed to FLush!!");
+                        }
+                }
+        }
+
         public static class Pair {
                 public int start,end;
                 public Pair(int a,int b) {
@@ -176,15 +231,18 @@ public class DBSystem {
         }
 
         public static class Page {
-                String page;
-                Integer pageNumber;
-                Pair limit;
-                int currentSize,tableNumber;
-                
-                public Page(int id,int tableNumber,int start,int end) {
+                public String page;
+                public Integer pageNumber;
+                public Pair limit;
+                public int currentSize,tableNumber;
+                public String tableName;
+                public Page(int id,int tableNumber,int start,int end, String tableName) {
                         this.pageNumber = id;
                         this.tableNumber = tableNumber;
                         this.limit = new Pair(start, end);
+                        this.tableName = tableName;
+                        this.currentSize = 0;
+                        this.page = "";
                 }
         }
 
@@ -193,9 +251,11 @@ public class DBSystem {
                 DBSystem d = new DBSystem();
                 d.readConfig("./config.txt");
                 d.populateDBInfo();
-                d.getRecord("employee", 1);
-                d.getRecord("employee", 0);
+                d.getRecord("student", 1);
+                d.getRecord("student", 0);
                 d.insertRecord("employee", "\"1\",\"abc\"");
+                d.insertRecord("student","\"3\",\"Name 3\",\"UG2kX\"");
+                d.flushPages();
                 //System.out.println(d.PageSize);
         }
 }
